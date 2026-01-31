ef2b1a0d2404f231d378d1183de3cb6d7eeabbf3
Feihu Yan <fy538@nyu.edu>
2026-01-31 13:39:13 -0500

    fix: allow login with email instead of username for JWT auth

--- backend/apps/auth_app/urls.py
+++ backend/apps/auth_app/urls.py
@@ -2,13 +2,13 @@
 Authentication URLs
 """
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from . import views
 
 urlpatterns = [
-    # JWT authentication
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # JWT authentication (with email support)
+    path('token/', views.EmailTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     # User profile
--- backend/apps/auth_app/views.py
+++ backend/apps/auth_app/views.py
@@ -1,11 +1,39 @@
 """
 Authentication views
 """
-from rest_framework import generics
+from rest_framework import generics, serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.models import User
 
 from .serializers import UserSerializer
+
+
+class EmailTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """Custom serializer that allows login with email instead of username"""
+    username_field = 'email'
+    
+    def validate(self, attrs):
+        # Get email from the request
+        email = attrs.get('email')
+        password = attrs.get('password')
+        
+        # Find user by email
+        try:
+            user = User.objects.get(email=email)
+            # Replace email with username for the parent class
+            attrs['username'] = user.username
+        except User.DoesNotExist:
+            pass  # Let the parent class handle the error
+        
+        # Call parent validation with username
+        return super().validate(attrs)
+
+
+class EmailTokenObtainPairView(TokenObtainPairView):
+    """Custom token view that accepts email instead of username"""
+    serializer_class = EmailTokenObtainPairSerializer
 
 
 class CurrentUserView(generics.RetrieveAPIView):
@@ -15,7 +43,3 @@ class CurrentUserView(generics.RetrieveAPIView):
     
     def get_object(self):
         return self.request.user
-
-
-# JWT token views are provided by rest_framework_simplejwt
-# We can customize them here if needed
