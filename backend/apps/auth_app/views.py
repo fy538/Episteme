2a0f25bfe0eb677be202c8c21be50a56d9dd7eae
Feihu Yan <fy538@nyu.edu>
2026-01-31 13:48:56 -0500

    fix: improve email authentication to properly convert email to username

--- backend/apps/auth_app/views.py
+++ backend/apps/auth_app/views.py
@@ -12,22 +12,27 @@ from .serializers import UserSerializer
 
 class EmailTokenObtainPairSerializer(TokenObtainPairSerializer):
     """Custom serializer that allows login with email instead of username"""
+    email = serializers.EmailField(required=True)
+    username = None  # Remove username field
     
     def validate(self, attrs):
-        # The frontend sends 'email', but the parent class expects 'username'
+        # Get email from the request
         email = attrs.get('email')
         password = attrs.get('password')
         
-        if email:
-            try:
-                user = User.objects.get(email=email)
-                # Replace 'email' with 'username' so authenticate() works
-                attrs[self.username_field] = user.username
-            except User.DoesNotExist:
-                # If email not found, let it proceed to fail naturally 
-                # so we don't leak information about which emails exist
-                pass
+        # Find user by email and convert to username
+        try:
+            user = User.objects.get(email=email)
+            # Replace email with username for the parent class
+            attrs['username'] = user.username
+            del attrs['email']  # Remove email so parent doesn't get confused
+        except User.DoesNotExist:
+            # If user doesn't exist, set a dummy username so parent can handle the error properly
+            attrs['username'] = email
+            if 'email' in attrs:
+                del attrs['email']
         
+        # Call parent validation with username
         return super().validate(attrs)
 
 
